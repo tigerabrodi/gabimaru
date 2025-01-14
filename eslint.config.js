7d24fff3fc1ab7f82f02419df73960129d5df05f
Tiger Abrodi <tigerabrodi@gmail.com>
2025-01-14 15:00:20 +0100

    chore: fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,47 @@
-import js from "@eslint/js";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parseroptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-        tsconfigrootdir: import.meta.dirname,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
       },
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
-      "react-refresh/only-export-components": [
-        "warn",
+      ...react.configs['jsx-runtime'].rules,
+      'react-refresh/only-export-components': [
+        'warn',
         { allowConstantExport: true },
       ],
-      "no-await-in-loop": "error",
-      "@typescript-eslint/array-type": ["error", { default: "generic" }],
-      "@typescript-eslint/naming-convention": [
-        "error",
+      'no-await-in-loop': 'error',
+      '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+      '@typescript-eslint/naming-convention': [
+        'error',
         {
-          selector: "variable",
-          types: ["boolean"],
-          format: ["PascalCase"],
-          prefix: ["is", "should", "has", "are", "can", "was"],
+          selector: 'variable',
+          types: ['boolean'],
+          format: ['PascalCase'],
+          prefix: ['is', 'should', 'has', 'are', 'can', 'was'],
         },
       ],
     },
   }
-);
+)
